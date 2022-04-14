bd5fd49ccc3bf4678a8184a32f6da75f30cf1d24
Daniel Alberto Ossandón Rojas <ossandonrojasdaniel@gmail.com>
2022-04-13 23:38:15 -0400

    modificacion 4

--- app/src/main/java/com/prodan/timper/MainActivity.java
+++ app/src/main/java/com/prodan/timper/MainActivity.java
@@ -2,14 +2,44 @@ package com.prodan.timper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int DURACION_SPLASH = 6000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSplash();
 
     }
+
+    private void getSplash() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Menu.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, DURACION_SPLASH);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
