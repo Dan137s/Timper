2b0fb5556a24fb664835f5740abfc90d68d38889
Daniel Alberto Ossandón Rojas <ossandonrojasdaniel@gmail.com>
2022-04-13 22:29:53 -0400

    Inicio version 0.1 de timper

---
+++ app/src/main/java/com/prodan/timper/MainActivity.java
@@ -0,0 +1,15 @@
+package com.prodan.timper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        s
+    }
+}
